d02c104e375a9578c41872c3ad62cb91c49ae00c
Aadarsh <dubeyaadarsh637@gmail.com>
2026-01-08 11:13:44 +0530

    add

---
+++ string/sort_vowel_in_string_2785.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+string sortVowel(string s){
+    vector<int> lower(26,0);
+    vector<int> upper(26,0);
+    for(int i=0;i<s.size();i++){
+         // lower a e i o u 
+        if(s[i]=='a'|| s[i]=='e'|| s[i]=='i'||s[i]=='o'||s[i]=='u'){
+            lower[s[i]-'a']++;
+            s[i]='#';
+        }else if(s[i]=='A'|| s[i]=='E'|| s[i]=='I'||s[i]=='O'||s[i]=='U'){
+            upper[s[i]-'A']++;
+            s[i]='#';
+        }
+    }
+    string vowel;
+    for(int i=0;i<26;i++){
+        char c ='A'+i;
+        while(upper[i]){
+            vowel+=c;
+            upper[i]--;
+        }
+    }
+
+    for(int i=0;i<26;i++){
+        char c ='a'+i;
+        while(lower[i]){
+            vowel+=c;
+            lower[i]--;
+        }
+    }
+    int first=0,second=0;
+    while(second<vowel.size()){
+        if(s[first]=='#'){
+            s[first]=vowel[second];
+            second++;
+        }
+        first++;
+    }
+    return s;
+} 
+int main() {
+    string s = "lEetcOde";
+    cout<<sortVowel(s);
+    
+    return 0;
+}
